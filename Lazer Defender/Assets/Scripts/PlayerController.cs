4d98dd1a42982118479a6f63ab0ae587bef4f808
Regis <X-lem@hotmail.com>
2017-01-18 13:32:28 -0800

    Update

--- Lazer Defender/Assets/Scripts/PlayerController.cs
+++ Lazer Defender/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,18 @@ public class PlayerController : MonoBehaviour {
     public float movementSpeed = 15.0f;
     float xMax;
     float xMin;
+    public float padding = 1f;
 
 
     // Use this for initialization
     void Start() {
 
+        float zDistance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, zDistance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, zDistance));
+
+        xMin = leftMost.x + padding;
+        xMax = rightMost.x - padding;
     }
 
     // Update is called once per frame
@@ -22,17 +29,15 @@ public class PlayerController : MonoBehaviour {
 
     void MoveShip() {
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-
+        if (Input.GetKey(KeyCode.LeftArrow)) {
             transform.position += Vector3.left * movementSpeed * Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.RightArrow)) {
             transform.position += Vector3.right * movementSpeed * Time.deltaTime;
-
         }
 
+        // Restrict the X movement of the spaceship
         float xPosition = Mathf.Clamp(transform.position.x, xMin, xMax);
-
-        transform.position = new Vector3(xPosition, transform.position.x, transform.position.y);
+        transform.position = new Vector3(xPosition, transform.position.y, transform.position.z);
     }
 }
